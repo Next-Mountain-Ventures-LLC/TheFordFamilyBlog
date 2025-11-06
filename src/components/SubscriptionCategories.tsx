bd1036d474a2e2768561cdd965a0465e4f3e02ca
new.website <noreply@new.website>
2025-11-06 15:45:33 +0000

    Automatic commit

--- src/components/SubscriptionCategories.tsx
+++ src/components/SubscriptionCategories.tsx
@@ -40,7 +40,7 @@ export default function SubscriptionCategories({ onChange, defaultSelected = ['f
     {
       id: 'business_ventures',
       name: 'Business Ventures',
-      description: 'Get updates on our family business endeavors and upcoming launches',
+      description: 'Help',
       icon: Store,
       special: true
     }
